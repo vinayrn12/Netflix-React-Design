087e68a67b071bc2a39676439d82844f8a5e4a3f
VINAY R NITTALI <vinayvrn12@gmail.com>
2021-09-29 11:24:24 +0530

    Added navbar

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.scss";
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () => {
+    return(
+        <div className='home'>
+            <Navbar/> 
+        </div>
+    )
+}
+
+export default Home;
